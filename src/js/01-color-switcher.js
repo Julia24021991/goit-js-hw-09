e5fa0d9d7f534b431e6c4c0ab355d0b7d7f8af27
Julia24021991 <yuliya.kovalyk@gmail.com>
2023-11-24 16:20:41 +0200

    commit2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerId = null;
 
+btnStart.addEventListener('click', () => {
+    timerId = setInterval(getRandomHexColor, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+    const hexColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    document.body.style.background = hexColor;
+}
